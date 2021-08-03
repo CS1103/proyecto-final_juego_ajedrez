6492bce62d7c706433f38477b804dd9000a47217
AronSantamaria <83294705+AronSantamaria@users.noreply.github.com>
2021-08-02 23:07:19 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,65 +1,231 @@
-#include "ajedrez.h"
+#include <SFML/Graphics.hpp>
+#include <time.h>
+#include "Connector.hpp"
+using namespace sf;
+using namespace std;
+
+
+int size = 56;
+Vector2f offset(28,28);
+
+
+// Piezas
+Sprite f[32];
+string position="";
+
+int board[8][8] =
+        {-1,-2,-3,-4,-5,-3,-2,-1,
+         -6,-6,-6,-6,-6,-6,-6,-6,
+         0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0,
+         6, 6, 6, 6, 6, 6, 6, 6,
+         1, 2, 3, 4, 5, 3, 2, 1};
+
+string toChessNote(Vector2f p)
+{
+    string s = "";
+    s += char(p.x/size+97);
+    s += char(7-p.y/size+49);
+    return s;
+}
+
+Vector2f toCoord(char a,char b)
+{
+    int x = int(a) - 97;
+    int y = 7-int(b)+49;
+    return Vector2f(x*size,y*size);
+}
+
+void move(string str)
+{
+    Vector2f oldPos = toCoord(str[0],str[1]);
+    Vector2f newPos = toCoord(str[2],str[3]);
+
+    for(int i=0;i<32;i++)
+        if (f[i].getPosition()==newPos) f[i].setPosition(-100,-100);
+
+    for(int i=0;i<32;i++)
+        if (f[i].getPosition()==oldPos) f[i].setPosition(newPos);
+
+    //castling       //if the king didn't move
+    if (str=="e1g1") if (position.find("e1")==-1) move("h1f1");
+    if (str=="e8g8") if (position.find("e8")==-1) move("h8f8");
+    if (str=="e1c1") if (position.find("e1")==-1) move("a1d1");
+    if (str=="e8c8") if (position.find("e8")==-1) move("a8d8");
+}
+
+void loadPosition()
+{
+    int k=0;
+    for(int i=0;i<8;i++)
+        for(int j=0;j<8;j++)
+        {
+            int n = board[i][j];
+            if (!n){
+                continue;
+            }
+            int x = abs(n)-1;
+            int y = n>0?1:0;
+            f[k].setTextureRect( IntRect(size*x,size*y,size,size) );
+            f[k].setPosition(size*j,size*i);
+            k++;
+        }
+
+    for(int i=0;i<position.length();i+=5)
+        move(position.substr(i,4));
+}
+template <typename T>
+void whatClassIs(T t){
+    cout<<__PRETTY_FUNCTION__ <<endl;
+}
 
 int main(){
-    
-    //Definir jugadores
-    string jugador_blancas_nombre;
-    string jugador_negras_nombre;
+    //      Se crea la ventana
+    RenderWindow window(VideoMode(504, 504), "The Chess! (press SPACE)");
 
-    cout << "Introduzca el nombre del jugador de las piezas blancas: " << endl;
-    cin >> jugador_blancas_nombre;
+    //      Motor de ajedrez
+    ConnectToEngine("stockfish.exe");
 
-    cout << "Introduzca el nombre del jugador de las piezas negras: " << endl;
-    cin >> jugador_blancas_nombre;
-
-    Jugador jugador_blancas = Jugador(jugador_blancas_nombre, 1, 0, 1);
-    Jugador jugador_negras = Jugador(jugador_negras_nombre, 0, 1, 0);
+    Texture t1,t2;
+    t1.loadFromFile("images/figures.png");
+    t2.loadFromFile("images/board.png");
 
 
-    //Definir piezas
-    Peon peon_blanco_1 = Peon({1,0}, {1,0}, 1, 0, 0);
-    Peon peon_blanco_2 = Peon({1,1}, {1,1}, 1, 0, 0);
-    Peon peon_blanco_3 = Peon({1,2}, {1,2}, 1, 0, 0);
-    Peon peon_blanco_4 = Peon({1,3}, {1,3}, 1, 0, 0);
-    Peon peon_blanco_5 = Peon({1,4}, {1,4}, 1, 0, 0);
-    Peon peon_blanco_6 = Peon({1,5}, {1,5}, 1, 0, 0);
-    Peon peon_blanco_7 = Peon({1,6}, {1,6}, 1, 0, 0);
-    Peon peon_blanco_8 = Peon({1,7}, {1,7}, 1, 0, 0);
+    for(int i=0;i<32;i++){
+        f[i].setTexture(t1);
+    }
+    //      Asignamos una variable a la imagen del tablero
+    Sprite sBoard(t2);
 
-    Peon peon_negro_1 = Peon({6,0}, {6,0}, 0, 1, 0);
-    Peon peon_negro_2 = Peon({6,1}, {6,1}, 0, 1, 0);
-    Peon peon_negro_3 = Peon({6,2}, {6,2}, 0, 1, 0);
-    Peon peon_negro_4 = Peon({6,3}, {6,3}, 0, 1, 0);
-    Peon peon_negro_5 = Peon({6,4}, {6,4}, 0, 1, 0);
-    Peon peon_negro_6 = Peon({6,5}, {6,5}, 0, 1, 0);
-    Peon peon_negro_7 = Peon({6,6}, {6,6}, 0, 1, 0);
-    Peon peon_negro_8 = Peon({6,7}, {6,7}, 0, 1, 0);
+    loadPosition();
 
-    Caballo caballo_blanco_1 = Caballo({0,1}, {0,1}, 1, 0, 0);
-    Caballo caballo_blanco_2 = Caballo({0,7}, {0,7}, 1, 0, 0);
+    for (int i = 0; i < 32; ++i) {
+        
+    }
 
-    Caballo caballo_negro_1 = Caballo({7,1}, {7,1}, 0, 1, 0);
-    Caballo caballo_negro_2 = Caballo({7,7}, {7,7}, 0, 1, 0);
+    bool isMove=false;
+    float dx=0, dy=0;
+    Vector2f oldPos,newPos;
+    string str;
+    int n=0;
 
-    Alfil alfil_blanco_casillas_negras = Alfil({0,2}, {0,2}, 1, 0, 0);
-    Alfil alfil_blanco_casillas_blancas = Alfil({0,6}, {0,6}, 1, 0, 0);
+    while (window.isOpen()){
+        Vector2i pos = Mouse::getPosition(window) - Vector2i(offset);
 
-    Alfil alfil_negro_casillas_blancas = Alfil({7,1}, {7,1}, 0, 1, 0);
-    Alfil alfil_negro_casillas_negras = Alfil({7,6}, {7,6}, 0, 1, 0);
 
-    Torre torre_blanca_1 = Torre({0,0}, {0,0}, 1, 0, 0);
-    Torre torre_blanca_2 = Torre({0,8}, {0,8}, 1, 0, 0);
+        // Registrar el boton que se esta pulsando
+        Event e;
+        //De acuerdo al boton presionado se toma desicion
+        while (window.pollEvent(e)){
+            //      Cerrar ventana con boton "Close"
+            if (e.type == Event::Closed)
+                window.close();
 
-    Torre torre_negra_1 = Torre({7,0}, {7,0}, 0, 1, 0);
-    Torre torre_negra_2 = Torre({7,8}, {7,8}, 0, 1, 0);
+            /*
+             *
+             *
+             *
+            if (e.type == Event::KeyPressed){
+                if (e.key.code == Keyboard::BackSpace){
+                    if (position.length()>6) position.erase(position.length()-6,5); loadPosition();
+                }
+             }
 
-    Dama dama_blanca = Dama({0,3}, {0,3}, 1, 0, 0);
+             */
+            //      Al presionar boton
+            if (e.type == Event::MouseButtonPressed)
 
-    Dama dama_negra = Dama({7,3}, {7,3}, 0, 1, 0);
+                //      Definir actividad al pulsar boton izquierdo del mouse
+                if (e.key.code == Mouse::Left)
+                    for(int i=0;i<32;i++)
+                        if (f[i].getGlobalBounds().contains(pos.x,pos.y)){
+                            if (i >= 16){
+                                isMove=true;
+                                n=i;
 
-    Rey rey_blanco = Rey({0,4}, {0,4}, 1, 0, 0);
-    
-    Rey rey_negro = Rey({7,4}, {7,4}, 0, 1, 0);
+                                dx=pos.x - f[i].getPosition().x;
+                                dy=pos.y - f[i].getPosition().y;
+                                oldPos  =  f[i].getPosition();
+                            }
+
+
+                        }
+
+            //          Al soltar boton
+            if (e.type == Event::MouseButtonReleased){
+                if (e.key.code == Mouse::Left){
+                    isMove=false;
+                    Vector2f p = f[n].getPosition() + Vector2f(size/2,size/2);
+                    newPos = Vector2f( size*int(p.x/size), size*int(p.y/size) );
+                    str = toChessNote(oldPos)+toChessNote(newPos);
+                    move(str);
+
+
+
+
+                    if (oldPos!=newPos) position+=str+" ";
+
+                    //          Poner limites al moviento;
+                    f[n].setPosition(newPos);
+                    whatClassIs(f[n]);
+                }
+            }
+
+        }
+
+        //comp move
+        if (Keyboard::isKeyPressed(Keyboard::Space))
+        {
+            str =  getNextMove(position);
+
+            oldPos = toCoord(str[0],str[1]);
+            newPos = toCoord(str[2],str[3]);
+
+            for(int i=0;i<32;i++) if (f[i].getPosition()==oldPos) n=i;
+
+            /////animation///////
+            for(int k=0;k<50;k++)
+            {
+                Vector2f p = newPos - oldPos;
+                f[n].move(p.x/50, p.y/50);
+                window.draw(sBoard);
+                for(int i=0;i<32;i++) f[i].move(offset);
+                for(int i=0;i<32;i++) window.draw(f[i]); window.draw(f[n]);
+                for(int i=0;i<32;i++) f[i].move(-offset);
+                window.display();
+            }
+
+            move(str);  position+=str+" ";
+            f[n].setPosition(newPos);
+        }
+
+        if (isMove) f[n].setPosition(pos.x-dx,pos.y-dy);
+
+        window.clear();
+
+        //      Pintar tablero
+        window.draw(sBoard);
+
+        //      Pintar piezas
+        for(int i=0;i<32;i++){
+            f[i].move(offset);
+        }
+        for(int i=0;i<32;i++){
+            window.draw(f[i]);
+            window.draw(f[n]);
+        }
+        for(int i=0;i<32;i++){
+            f[i].move(-offset);
+        }
+
+
+        //      Efectuar cambios
+        window.display();
+    }
+
+    CloseConnection();
 
     return 0;
 }
